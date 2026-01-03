87152bc41f70b5946d47448034db141a9e16fd5e
Riti Moradiya <ritimoradiya1102@gmail.com>
2026-01-02 22:14:01 -0500

    Segregate logs into separate log groups by level

--- src/config/statsd.js
+++ src/config/statsd.js
@@ -1,5 +1,6 @@
 // src/config/statsd.js
 const StatsD = require('node-statsd');
+const logger = require('./logger');
 
 // Create StatsD client
 const statsd = new StatsD({
@@ -10,9 +11,9 @@ const statsd = new StatsD({
   mock: process.env.NODE_ENV === 'test', // Don't send metrics during testing
 });
 
-// Handle errors
+// Handle errors with logger instead of console.error
 statsd.socket.on('error', (error) => {
-  console.error('StatsD socket error:', error);
+  logger.error('StatsD socket error', { error: error.message, stack: error.stack });
 });
 
 module.exports = statsd;
